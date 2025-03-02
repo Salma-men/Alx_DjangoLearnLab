c38b9d5044d53c4c85579bf96deee15591287bb8
Salma Mennani <men.salmaa@gmail.com>
2025-03-02 05:19:20 +0000

    fixing errors

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library, Book   # Ensure Library is imported
