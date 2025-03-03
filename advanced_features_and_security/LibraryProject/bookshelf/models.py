ed3537d264ac23222ca2af14fdc652a323d28478
Salma Mennani <men.salmaa@gmail.com>
2025-03-03 02:05:51 +0000

    second task

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -7,3 +9,29 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username must be set')
+        user = self.model(username=username, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, date_of_birth, profile_photo, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()  # Attach the custom user manager
+
+    def __str__(self):
+        return self.username
