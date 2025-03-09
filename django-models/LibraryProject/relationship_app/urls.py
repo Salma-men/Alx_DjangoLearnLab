fcc14d6dd4064a814aaf71729133c1a50b2ea323
Salma Mennani <men.salmaa@gmail.com>
2025-03-09 01:10:05 +0000

    new fixings

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views  # Import views to use views.register
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books
+from .views import list_books, LibraryDetailView 
 
 urlpatterns = [
     # Existing Routes for Books and Library Details
     path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     
     # Authentication Routes
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
 from .models import UserProfile 
+from django.views.generic import DetailView
+from .models import Book, Library  # Ensure Library is imported!
 
 # Function-Based View for Listing Books
 def list_books(request):
@@ -19,6 +21,7 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
 
 # User Login View
 def user_login(request):
