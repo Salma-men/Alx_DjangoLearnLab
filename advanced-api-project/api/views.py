eef5edc50b6ea6f7dd66a7d68c1bcdaddf32be97
Salma Mennani <men.salmaa@gmail.com>
2025-03-16 05:37:58 +0000

    added IsAuthenticatedOrReadOnly, IsAuthenticated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,31 +2,36 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # ListView - Retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow anyone to read, but only authenticated users can create.
 
 # DetailView - Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow reading for everyone.
+
 
 # CreateView - Add a new book (Restricted to authenticated users)
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # Only logged-in users can create
+    permission_classes = [IsAuthenticated]  # Only authenticated users can create.
+
 
 # UpdateView - Modify an existing book (Restricted to authenticated users)
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]  # Only authenticated users can update.
 
 # DeleteView - Remove a book (Restricted to authenticated users)
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]  # Only authenticated users can delete.
