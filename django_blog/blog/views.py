19b7d1a3c0c6238038583021d73f4242611bf6af
Salma Mennani <men.salmaa@gmail.com>
2025-03-30 20:44:32 +0000

    Implemented CRUD functionality for blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,6 +12,10 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+
 
 # Registration View
 def register(request):
@@ -47,46 +51,39 @@ def profile(request):
 def home(request):
     return render(request, 'blog/index.html')
 
-# List of posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    ordering = ['-published_date']
 
-# Detail view of a single post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
 
-# Create new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    form_class = PostForm
     template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post_list')
+    fields = ['title', 'content']
 
     def form_valid(self, form):
-        form.instance.author = self.request.user  # Set author to logged-in user
+        form.instance.author = self.request.user  # Set the current user as the author
         return super().form_valid(form)
 
-# Update an existing post
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    form_class = PostForm
     template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post_list')
+    fields = ['title', 'content']
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(author=self.request.user)  # Only allow users to update their posts
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only the author to update
 
-# Delete a post
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
+    success_url = reverse_lazy('post-list')
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(author=self.request.user)  # Only allow users to delete their posts
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only the author to delete
