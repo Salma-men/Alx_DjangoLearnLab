8a95dfd1e92e4848901654bf23ab0b80fd8ae378
Salma Mennani <men.salmaa@gmail.com>
2025-03-30 21:12:35 +0000

    Fixed RegisterSerializer: Added CharField for password

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,14 +10,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # This ensures password is write-only
+    password = serializers.CharField(  # <-- This is what the error is complaining about!
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(  # Ensure using `get_user_model()`
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
